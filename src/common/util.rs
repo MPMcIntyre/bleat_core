b3ad699c50eb6e946981c69e8b67043a72667141
Michael <mpmcintyre@yahoo.com>
2024-05-15 10:32:05 +0200

    More notices

--- src/common/util.rs
+++ src/common/util.rs
@@ -1,9 +1,13 @@
 // bleat_core Source Code File
-//
-// Copyright 2020 Nonpolynomial. All rights reserved.
+// Copyright 2024 Alt-Innovations. All rights reserved.
 //
 // Licensed under the BSD 3-Clause license. See LICENSE file in the project root
 // for full license information.
+//
+// Some portions of this file are taken and/or modified from btleplug
+// (https://github.com/deviceplug/btleplug), BSD 3-Clause license under the
+// following copyright:
+// Copyright 2020 Nonpolynomial Labs LLC.
 
 use crate::api::ValueNotification;
 use futures::stream::{Stream, StreamExt};
